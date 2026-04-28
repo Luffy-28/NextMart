bca31fe119ea012c9cdc19f86e1a38f474686944
Shankar <thakurishankar159@gmail.com>
2026-04-28 17:35:33 +1000

    Add SMTP configuration and email verification helper

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -8,5 +8,20 @@ export const config = {
     secret: process.env.JWT_SECRET,
     expiresIn: process.env.JWT_EXPIRES_IN,
   },
-  salt: process.env.SALT,
+  salt: parseInt(process.env.SALT),
+  smtp: {
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: process.env.SMTP_SECURE,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  },
+  aws: {
+    accessKey: process.env.AWS_ACCESS_KEY_ID,
+    secretKey: process.env.AWS_SECRET_ACCESS_KEY,
+    bucketName: process.env.AWS_BUCKET_NAME,
+    region: process.env.AWS_REGION,
+  },
 };
